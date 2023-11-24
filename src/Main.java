2ea8589e5ece50e496d350dd312096472997401c
dasha T <olga061200@icloud.com>
2023-11-24 14:02:36 +0300

    2nd task done

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,24 @@
 
 public class Main {
     public static void main(String[] args) {
-// Задание 1
-        int tiket = 15_000;
-        int onemile = 20;
+//Task 1
+        //      int tiket = 15_000;
+        //      int onemile = 20;
 
-        int mile = tiket / onemile;
-        System.out.println(mile);
-// Задание 2
-        
+        //      int mile = tiket / onemile;
+
+        //      System.out.println(mile);
+//Task 2
+        int initial = 100;
+        int replenishment = 1000;
+        int bonus;
+        if (replenishment >= 1000) {
+            bonus = replenishment / 100;
+        } else {
+            bonus = 0;
+        }
+        int account = initial + replenishment + bonus;
+        System.out.println("Account =" + account);
     }
 }
+
