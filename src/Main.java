f0a248fd7221ee48bee9a7fed937276cdc808e0c
dasha T <olga061200@icloud.com>
2023-11-24 12:51:25 +0300

    1st task done

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,13 @@
+
 public class Main {
+    public static void main(String[] args) {
+// Задание 1
+        int tiket = 15_000;
+        int onemile = 20;
+
+        int mile = tiket / onemile;
+        System.out.println(mile);
+// Задание 2
+        
+    }
 }
