22b5a037f7fb6ae75b379a794f54ca5eb19b7b77
dasha T <olga061200@icloud.com>
2023-12-01 15:45:58 +0300

    Second trial

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,11 @@
 
 public class Main {
     public static void main(String[] args) {
-//Task 1
-        //      int tiket = 15_000;
-        //      int onemile = 20;
-
-        //      int mile = tiket / onemile;
-
-        //      System.out.println(mile);
 //Task 2
         int initial = 100;
         int replenishment = 1000;
         int bonus;
-        if (replenishment >= 1000) {
+        if (replenishment > 1000) {
             bonus = replenishment / 100;
         } else {
             bonus = 0;
---
+++ src/Second.java
@@ -0,0 +1,9 @@
+public class Second {
+    public static void main(String[] args) {
+//Task 1
+       int tiket = 15_000;
+       int onemile = 20;
+       int mile = tiket / onemile;
+       System.out.println(mile);
+    }
+}
