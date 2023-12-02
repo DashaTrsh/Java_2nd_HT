16abd46bf6ce6590938c01559d7a6bfa3989ae74
dasha T <olga061200@icloud.com>
2023-12-02 13:21:11 +0300

    CamelCase

--- src/Second.java
+++ src/Second.java
@@ -2,8 +2,8 @@ public class Second {
     public static void main(String[] args) {
 //Task 1
        int tiket = 15_000;
-       int onemile = 20;
-       int mile = tiket / onemile;
+       int oneMile = 20;
+       int mile = tiket / oneMile;
        System.out.println(mile);
     }
 }
